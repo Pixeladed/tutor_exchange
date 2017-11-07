1fab150fa74577da149f8d3cbecc612c1bfd0cc4
Pixeladed <vietminhle98@gmail.com>
2017-11-07 13:30:18 +0800

    use connection pool instead

--- api/server.js
+++ api/server.js
@@ -22,7 +22,7 @@ app.set('secret', config.secret);
 /*var cheerio = require('cheerio'),
     $ = cheerio.load(fs.readFile(__dirname+'/app/templates/verifyEmail.html'));*/
 
-var connection = mysql.createConnection(config.mysqlSettings);
+var connection = mysql.createPool(config.mysqlSettings);
 connection.connect (function(error) {
   if (!!error) {
     console.log(error);
@@ -30,26 +30,6 @@ connection.connect (function(error) {
     console.log('Connected to mysql database');
   }
 });
-
-function handleDisconnect() {
-  connection = mysql.createConnection(config.mysqlSettings);
-  connection.connect (function(error) {
-    if (!!error) {
-      console.log(error);
-    } else {
-      console.log('Connected to mysql database');
-    }
-  });
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                         
-    } else {                                     
-      throw err;                                  
-    }
-  });
-}
-handleDisconnect();
 
 //from http://stackoverflow.com/questions/39489229/pass-variable-to-html-template-in-nodemailer
 var readHTMLFile = function(path, callback) {
@@ -96,6 +76,7 @@ app.use('/auth/register', function(req, res) {
     passwordSalt: passhashsalt.salt,  //<- Need to store salt with password.  Salt only protects against rainbow table attacks.
   };
 
+pool.getConnection(function(err, connection) {
   connection.query('SELECT * FROM user WHERE userID = ?', post.userID, function(err, rows, fields) {
     if (err) {
       console.log(err);
@@ -1390,6 +1371,8 @@ function clearResetToken(userID) {
       return;
     });
 }
+
+});
 
 /** [from https://code.ciphertrick.com/2016/01/18/salt-hash-passwords-using-nodejs-crypto/]
  * generates random string of characters i.e salt
